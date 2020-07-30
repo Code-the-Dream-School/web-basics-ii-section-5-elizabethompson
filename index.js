08bc0be0b8c8e599d2d338ffe710076fe6ea5eb8
Elizabeth Thompson <lizthompson14@gmail.com>
2020-07-30 01:04:36 -0400

    feat: complete assignment

--- index.js
+++ index.js
@@ -1,38 +1,119 @@
+const currentPlayerEl = document.getElementById('turn_player')
 
-//------------------------ Game Project---------------------------
-//Do you remember the game Battleship we created before? well .... it is time to make it with the DOM!!
-//We are providing you with the design of a board (in the DOM) for a player1, you have to create the board for the player2 using the id property 'board_player2' -> it is the second list (ul) in your index.html file
-//First ask the players for their names (use propmt)
-//Now each time the turn player clicks on any cell of the opponent's board (you have to verify if the player is clicking the right board) the program needs to verify if there is an opponent's ship in that cell. If it is then the opponent has one less ship
-//We want you to store the data of each player in two Player objects. Each object has to store: name, remaining boats, and their respective board.
-//Each board needs to be initialized randomly with '0' and four '1' wich means the state of the cell. Numbers 1 are representing the 4 positions of the player's ships
-//Also we want you to display the name of the turn player in the tag that has the id 'turn_player'. And if there is a winner  a text with: 'Congratulationes {name_player}!! you win'
-//in the index.html file you are going to find 4 more ids: 'name_player1' , 'name_player2' , 'ships_player1' , 'ships_player2'. We want to see the information of each player in the respective elements
-//As our previous Battleship, the winner is the player that hits the 4 opponent's ships first
-//one more Thing create a 'reset' and a 'new game' buttons as childs of the element with the id 'buttons'. the reset button has to start the game again and the new game create a new game with new players and a new random board.
+const player1 = {
+  name: prompt("What's your name, player 1?"),
+  numShips: 4,
+  gameEl: {
+    board: document.getElementById('board_player1'),
+    name: document.getElementById('name_player1'),
+    ships: document.getElementById('ships_player1'),
+  },
+}
 
-const board_Player1 = document.getElementById('board_player1');
+const player2 = {
+  name: prompt("What's your name, player 2?"),
+  numShips: 4,
+  gameEl: {
+    board: document.getElementById('board_player2'),
+    name: document.getElementById('name_player2'),
+    ships: document.getElementById('ships_player2'),
+  },
+}
 
-for (var x = 0; x < 4; x++) {
+let currentPlayer
 
-    const li = document.createElement('li'); // creating childs for the list (board), in this case represent a row number 'x' of the board
+const setCurrentPlayer = (player) => {
+  currentPlayer = player
+  currentPlayerEl.innerHTML = player.name
+}
+
+// default current player to player 1
+setCurrentPlayer(player1)
+
+const strikeBoard = (event) => {
+  // retrieve the clicked element from the click event, which is the <div> board cell
+  const cellEl = event.target
+  // retrieve the "grandparent" of the clicked board cell, which is the <ul> game board
+  const boardEl = cellEl.parentNode.parentNode
+
+  // check if cell belongs to the current player's board, if so return (stop)
+  if (boardEl === currentPlayer.gameEl.board) return
+
+  // opponent
+  const opponent = currentPlayer === player1 ? player2 : player1
+
+  // do the game
+  if (cellEl.value === 1) {
+    opponent.numShips--
+    opponent.gameEl.ships.innerText = opponent.numShips
+
+    // change the color of the clicked board cell
+    cellEl.style.backgroundColor = '#e71d36'
+  } else {
+    // change the color of the clicked board cell
+    cellEl.style.backgroundColor = '#5e6472'
+  }
+
+  if (opponent.numShips <= 0) {
+    currentPlayerEl.innerHTML = `Congratulations, ${currentPlayer.name}! You win.`
+    cellEl.removeEventListener('click', strikeBoard)
+    return
+  }
+
+  // switch the current player
+  setCurrentPlayer(opponent)
+}
+
+const drawBoard = (boardEl) => {
+  for (var x = 0; x < 4; x++) {
+    const li = document.createElement('li')
 
     for (var y = 0; y < 4; y++) {
-      const cell = document.createElement('div');
-      cell.className = "square"; // adding css properties to make it looks like a square
-      cell.textContent = `${x},${y}`;  // saves the coordinates as a string value 'x,y'
-      cell.value = 0;//state of the cell
+      const cell = document.createElement('div')
+      cell.className = 'square'
+      cell.textContent = `${x},${y}`
+      cell.value = 0
+      cell.addEventListener('click', strikeBoard)
 
-      //this function adds the click event to each cell
-      cell.addEventListener( 'click', (e) => {
-          let cell = e.target; // get the element clicked
-          console.log( cell.textContent) //display the coordinates in the console
-          cell.style.visibility = 'hidden';// this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
-          //cell.style.background ="purple"; //with this propertie you can change the background color of the clicked cell. try comment the line bellow and uncomment this line. Do not forget to save this file and refresh the borwser to see the changes
-      });
-
-      li.appendChild(cell); //adding each cell into the row number x
+      li.appendChild(cell)
     }
 
-     board_Player1.appendChild(li); //adding each row into the board
+    boardEl.appendChild(li)
+  }
 }
+
+const placeShips = (boardEl) => {
+  const randomCoordinates = { x: 0, y: 0 }
+  let addedShipCount = 0
+
+  while (addedShipCount < 4) {
+    let { x, y } = randomCoordinates
+    x = Math.floor(Math.random() * Math.floor(4))
+    y = Math.floor(Math.random() * Math.floor(4))
+
+    let boardRow = boardEl.getElementsByTagName('li')[x]
+    let boardCell = boardRow.getElementsByTagName('div')[y]
+
+    if (boardCell.value !== 1) {
+      boardCell.value = 1
+      console.log(boardCell.innerText) // log out ship coords for testing
+      addedShipCount++
+    }
+  }
+}
+
+// display player names on page
+player1.gameEl.name.innerText = player1.name
+player2.gameEl.name.innerText = player2.name
+
+// display player number of ships on page
+player1.gameEl.ships.innerText = player1.numShips
+player2.gameEl.ships.innerText = player2.numShips
+
+// draw game board for both players
+drawBoard(player1.gameEl.board)
+drawBoard(player2.gameEl.board)
+
+// place ships on game board for both players
+placeShips(player1.gameEl.board)
+placeShips(player2.gameEl.board)
